766ea367a5c75690f7db1d439a8c44d490557604
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2025-07-25 20:34:58 +0800

    feat: add `group_col` parameter to `autoplot.glystats_pca_res()`
    This function now makes the best of information to get groups from the result.

--- R/glystats-pca.R
+++ R/glystats-pca.R
@@ -13,6 +13,12 @@
 #'   If provided, the plot will be colored by group.
 #'   Only applicable to "individual" and "biplot" types.
 #'   Passed to the `habillage` paramter of [factoextra::fviz_pca_ind()] or [factoextra::fviz_pca_biplot()].
+#' @param group_col A character string specifying where to find the group information.
+#'   If you uses [glystats::gly_pca()] on a [glyexp::experiment()] to get the result,
+#'   sample information has already been added to the result.
+#'   In this case, you can specify the column name in the sample information tibble
+#'   to be used for coloring.
+#'   If not provided, this function will try "group".
 #' @param ... Other arguments passed to factoextra functions.
 #'   - For "screeplot", see [factoextra::fviz_screeplot()].
 #'   - For "individual", see [factoextra::fviz_pca_ind()].
@@ -23,7 +29,7 @@
 #' @seealso [factoextra::fviz_screeplot()], [factoextra::fviz_pca_ind()],
 #'   [factoextra::fviz_pca_var()], [factoextra::fviz_pca_biplot()]
 #' @export
-autoplot.glystats_pca_res <- function(object, type = "individual", groups = NULL, ...) {
+autoplot.glystats_pca_res <- function(object, type = "individual", groups = NULL, group_col = NULL, ...) {
   .check_pkg_available("factoextra")
   checkmate::assert_choice(type, c("screeplot", "individual", "variables", "biplot"))
   checkmate::assert(
@@ -32,15 +38,7 @@ autoplot.glystats_pca_res <- function(object, type = "individual", groups = NULL
     checkmate::check_null(groups)
   )
 
-  if (!is.null(groups)) {
-    if (length(groups) != length(unique((object$tidy_result$samples$sample)))) {
-      cli::cli_abort(c(
-        "Length of {.arg groups} must be equal to the number of samples.",
-        "i" = "Number of samples: {.val {length(unique((object$tidy_result$samples$sample))}}.",
-        "i" = "Length of {.arg groups}: {.val {length(groups)}}."
-      ))
-    }
-  }
+  groups <- .get_pca_groups(object, groups, group_col)
 
   switch(
     type,
@@ -49,6 +47,45 @@ autoplot.glystats_pca_res <- function(object, type = "individual", groups = NULL
     variables = .plot_pca_variables(object, ...),
     biplot = .plot_pca_biplot(object, groups, ...)
   )
+}
+
+.get_pca_groups <- function(object, groups, group_col) {
+  # Case 1: `groups` is specified: use `groups` directly and ignore `group_col`
+  if (!is.null(groups)) {
+    if (length(groups) != length(unique(object$tidy_result$samples$sample))) {
+      cli::cli_abort(c(
+        "Length of {.arg groups} must be equal to the number of samples.",
+        "i" = "Number of samples: {.val {length(unique(object$tidy_result$samples$sample))}}.",
+        "i" = "Length of {.arg groups}: {.val {length(groups)}}."
+      ))
+    }
+    return(groups)
+  }
+
+  # Case 2: `group_col` is specified: check existence, and extract from `object$tidy_result$samples`
+  if (!is.null(group_col)) {
+    if (!group_col %in% colnames(object$tidy_result$samples)) {
+      cli::cli_abort(c(
+        "Column {.val {group_col}} not found.",
+        "i" = "Does your `glyexp::experiment()` contain the column in the sample information tibble?"
+      ))
+    }
+    return(.get_groups_from_pca_res(object, group_col))
+  }
+
+  # Case 3: both are not specified: try "group"
+  if ("group" %in% colnames(object$tidy_result$samples)) {
+    return(.get_groups_from_pca_res(object, "group"))
+  }
+
+  # Case 4: neither is specified and "group" doesn't exist: return NULL
+  return(NULL)
+}
+
+.get_groups_from_pca_res <- function(object, group_col) {
+  object$tidy_result$samples %>%
+    dplyr::distinct(.data$sample, .data[[group_col]]) %>%
+    dplyr::pull(.data[[group_col]])
 }
 
 .plot_pca_screeplot <- function(object, ...) {
--- tests/testthat/test-glystats-pca.R
+++ tests/testthat/test-glystats-pca.R
@@ -1,4 +1,5 @@
 test_that("autoplot works for glystats_pca_res", {
+  set.seed(1234)
   suppressMessages(
     pca_res <- glystats::gly_pca(test_gp_exp)
   )
@@ -17,5 +18,155 @@ test_that("autoplot works for glystats_pca_res", {
   vdiffr::expect_doppelganger(
     "autoplot.glystats_pca_res_biplot",
     autoplot(pca_res, type = "biplot")
+  )
+})
+
+test_that("autoplot works for glystats_pca_res with groups parameter", {
+  set.seed(1234)
+  suppressMessages(
+    pca_res <- glystats::gly_pca(test_gp_exp)
+  )
+
+  # Test with explicit groups parameter
+  groups <- c("A", "A", "A", "B", "B", "B", "C", "C", "C", "D", "D", "D")
+
+  vdiffr::expect_doppelganger(
+    "autoplot.glystats_pca_res_individual_with_groups",
+    autoplot(pca_res, type = "individual", groups = groups)
+  )
+
+  vdiffr::expect_doppelganger(
+    "autoplot.glystats_pca_res_biplot_with_groups",
+    autoplot(pca_res, type = "biplot", groups = groups)
+  )
+})
+
+test_that("autoplot works for glystats_pca_res with group_col parameter", {
+  set.seed(1234)
+  suppressMessages(
+    pca_res_with_group <- glystats::gly_pca(test_gp_exp)
+  )
+
+  # Test with group_col parameter (using existing "group" column)
+  vdiffr::expect_doppelganger(
+    "autoplot.glystats_pca_res_individual_with_group_col",
+    autoplot(pca_res_with_group, type = "individual", group_col = "group")
+  )
+
+  vdiffr::expect_doppelganger(
+    "autoplot.glystats_pca_res_biplot_with_group_col",
+    autoplot(pca_res_with_group, type = "biplot", group_col = "group")
+  )
+})
+
+test_that("autoplot automatically uses 'group' column when available", {
+  set.seed(1234)
+  suppressMessages(
+    pca_res_with_group <- glystats::gly_pca(test_gp_exp)
+  )
+
+  # Test automatic detection of "group" column
+  vdiffr::expect_doppelganger(
+    "autoplot.glystats_pca_res_individual_auto_group",
+    autoplot(pca_res_with_group, type = "individual")
+  )
+
+  vdiffr::expect_doppelganger(
+    "autoplot.glystats_pca_res_biplot_auto_group",
+    autoplot(pca_res_with_group, type = "biplot")
+  )
+})
+
+test_that("autoplot works without groups when no group info available", {
+  set.seed(1234)
+  suppressMessages(
+    pca_res_no_group <- test_gp_exp |>
+      glyexp::select_obs(-group) |>
+      glystats::gly_pca()
+  )
+
+  # Test without any group information
+  vdiffr::expect_doppelganger(
+    "autoplot.glystats_pca_res_individual_no_group",
+    autoplot(pca_res_no_group, type = "individual")
+  )
+
+  vdiffr::expect_doppelganger(
+    "autoplot.glystats_pca_res_biplot_no_group",
+    autoplot(pca_res_no_group, type = "biplot")
+  )
+})
+
+test_that("autoplot validates groups parameter correctly", {
+  set.seed(1234)
+  suppressMessages(
+    pca_res <- glystats::gly_pca(test_gp_exp)
+  )
+
+  # Test with wrong length groups
+  wrong_groups <- c("A", "B", "C")  # Should be 12 elements
+  expect_error(
+    autoplot(pca_res, groups = wrong_groups),
+    "Length of.*groups.*must be equal to the number of samples"
+  )
+})
+
+test_that("autoplot validates group_col parameter correctly", {
+  set.seed(1234)
+  suppressMessages(
+    pca_res <- glystats::gly_pca(test_gp_exp)
+  )
+
+  # Test with non-existent group_col
+  expect_error(
+    autoplot(pca_res, group_col = "nonexistent_column"),
+    "Column.*nonexistent_column.*not found"
+  )
+})
+
+test_that("groups parameter takes precedence over group_col", {
+  set.seed(1234)
+  suppressMessages(
+    pca_res <- glystats::gly_pca(test_gp_exp)
+  )
+
+  # When both groups and group_col are provided, groups should be used
+  groups <- c("X", "X", "X", "Y", "Y", "Y", "Z", "Z", "Z", "W", "W", "W")
+
+  vdiffr::expect_doppelganger(
+    "autoplot.glystats_pca_res_groups_precedence",
+    autoplot(pca_res, type = "individual", groups = groups, group_col = "group")
+  )
+})
+
+test_that("autoplot type validation works correctly", {
+  set.seed(1234)
+  suppressMessages(
+    pca_res <- glystats::gly_pca(test_gp_exp)
+  )
+
+  # Test invalid type
+  expect_error(
+    autoplot(pca_res, type = "invalid_type"),
+    "Must be element of set"
+  )
+})
+
+test_that("groups parameter accepts both factor and character", {
+  set.seed(1234)
+  suppressMessages(
+    pca_res <- glystats::gly_pca(test_gp_exp)
+  )
+
+  # Test with character groups
+  char_groups <- c("A", "A", "A", "B", "B", "B", "C", "C", "C", "D", "D", "D")
+  expect_no_error(
+    autoplot(pca_res, type = "individual", groups = char_groups)
+  )
+
+  # Test with factor groups
+  factor_groups <- factor(char_groups)
+  expect_no_error(
+    autoplot(pca_res, type = "individual", groups = factor_groups)
   )
 })
