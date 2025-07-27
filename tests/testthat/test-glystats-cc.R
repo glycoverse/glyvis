ac6d22aa251ae1a81e34dca7edceccd31bac7199
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2025-07-27 12:09:14 +0800

    fix: skip some tests
    Skip test for `gly_cc_res` on CI, CRAN, and when "ConsensusClusterPlus" is not installed.

--- tests/testthat/test-glystats-cc.R
+++ tests/testthat/test-glystats-cc.R
@@ -1,4 +1,8 @@
-test_that("autoplot raises error for glystats cc results", {
+skip_on_ci()
+skip_on_cran()
+
+  test_that("autoplot raises error for glystats cc results", {
+  skip_if_not_installed("ConsensusClusterPlus")
   cc_res <- suppressMessages(glystats::gly_consensus_clustering(test_gp_exp, max_k = 3, reps = 30))
   expect_error(autoplot(cc_res), "Can't plot consensus clustering results currectly.")
 })
