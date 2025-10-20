54dcb458aa7da00403298e25e4be14e42dd7b16f
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2025-10-20 13:56:25 +0800

    fix: dismiss messages during tests

--- tests/testthat/test-plot-roc.R
+++ tests/testthat/test-plot-roc.R
@@ -2,22 +2,19 @@ skip_on_ci()
 skip_on_cran()
 
 test_that("plot_roc works for glyexp_experiment with default parameters", {
+  exp <- test_gp_exp |>
+    glyexp::slice_head_var(n = 10) |>
+    glyexp::filter_obs(group %in% c("H", "C"))
   vdiffr::expect_doppelganger(
     "plot_roc",
-    test_gp_exp |>
-      glyexp::slice_head_var(n = 10) |>
-      glyexp::filter_obs(group %in% c("H", "C")) |>
-      plot_roc()
+    suppressMessages(plot_roc(exp))
   )
 })
 
 test_that("plot_roc works for glystats_roc_res with default parameters", {
-  vdiffr::expect_doppelganger(
-    "plot_roc_roc_res",
-    test_gp_exp |>
-      glyexp::slice_head_var(n = 10) |>
-      glyexp::filter_obs(group %in% c("H", "C")) |>
-      glystats::gly_roc() |>
-      plot_roc()
-  )
+  exp <- test_gp_exp |>
+    glyexp::slice_head_var(n = 10) |>
+    glyexp::filter_obs(group %in% c("H", "C"))
+  suppressMessages(roc_res <- glystats::gly_roc(exp))
+  vdiffr::expect_doppelganger("plot_roc_roc_res", plot_roc(roc_res))
 })
