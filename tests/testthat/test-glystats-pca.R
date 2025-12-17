d81b772ec4d3c94f1aa10c06e4cb63baa47fdc9c
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2025-12-17 17:07:32 +0800

    fix: dismiss warning in plot_pca
    The warning is caused by ggplot2 4.0.0 update. This warning is issued when printing the plot, so it cannot be easily dismissed.

--- tests/testthat/test-glystats-pca.R
+++ tests/testthat/test-glystats-pca.R
@@ -14,9 +14,11 @@ test_that("autoplot works for glystats_pca_res", {
     "autoplot.glystats_pca_res_variables",
     autoplot(pca_res, type = "variables")
   )
-  vdiffr::expect_doppelganger(
-    "autoplot.glystats_pca_res_screeplot",
-    autoplot(pca_res, type = "screeplot")
+  suppressWarnings(
+    vdiffr::expect_doppelganger(
+      "autoplot.glystats_pca_res_screeplot",
+      autoplot(pca_res, type = "screeplot")
+    )
   )
   vdiffr::expect_doppelganger(
     "autoplot.glystats_pca_res_biplot",
